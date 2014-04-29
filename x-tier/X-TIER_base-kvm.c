69ca8d4a849cec8b98aa193eb358ebe15b185935
Jonas Jelten <jj@sft.mx>
2014-04-29 23:54:56 +0200

    added helper files for switching between kernel and emulator build

---
+++ x-tier/X-TIER_base-kvm.c
@@ -0,0 +1,4 @@
+//use this file to build the xtier shared code kernelspace part
+
+#define DO_KERNEL_BUILD
+#include "X-TIER.c"
---
+++ x-tier/X-TIER_base-qemu.c
@@ -0,0 +1,4 @@
+//use this file to build the xtier shared code userspace part
+
+#define DO_USERSPACE_BUILD
+#include "X-TIER.c"
