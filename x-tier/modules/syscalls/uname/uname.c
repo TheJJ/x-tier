1d8d47474f9b9033f32a71d6a98f4dbcb3eb4979
Jonas Jelten <jj@sft.mx>
2014-08-25 23:55:42 +0200

    x-tier: fixed uname module code

--- x-tier/modules/syscalls/uname/uname.c
+++ x-tier/modules/syscalls/uname/uname.c
@@ -9,16 +9,7 @@ extern int sys_newuname(struct new_utsname *name);
 long uname(void) {
 	struct new_utsname result;
 
-	// TODO: alloc n cpy:
-	/*
-	char sysname[];
-	char nodename[];
-	char release[];
-	char version[];
-	char machine[];
-	*/
-
-	//int ret = sys_newuname(&result);
+	int ret = sys_newuname(&result);
 
 	data_transfer((char *)&result, sizeof(result));
 	return ret;
