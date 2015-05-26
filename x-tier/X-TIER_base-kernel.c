c2ff97e1e15847caa7302316dba792bc393b922c
Jonas Jelten <jj@sft.mx>
2015-05-27 00:17:28 +0200

    x-tier: removed old x-tier code and header files

--- x-tier/X-TIER_base-kernel.c
+++
@@ -1,4 +0,0 @@
-//use this file to build the xtier shared code kernelspace part
-
-#define DO_KERNEL_BUILD
-#include "X-TIER.c"
--- x-tier/X-TIER_base-userland.c
+++
@@ -1,4 +0,0 @@
-//use this file to build the xtier shared code userspace part
-
-#define DO_USERSPACE_BUILD
-#include "X-TIER.c"
